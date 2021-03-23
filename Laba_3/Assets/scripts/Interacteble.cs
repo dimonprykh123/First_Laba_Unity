02cdc70e836c62cfb4955d272645173f3a522fa5
dimonprykh123 <58978858+dimonprykh123@users.noreply.github.com>
2021-03-23 15:58:21 +0200

    laba 3

---
+++ Laba_3/Assets/scripts/Interacteble.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interacteble : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool _isFocus;
+    private PlayerCreature _player;
+    private bool _hasInterected;
+    [SerializeField] private float _interactionDistance;
+    public virtual float StopingDIstance
+    {
+        get { return _interactionDistance * 0.7f; }
+    }
+    public void OnFocus(PlayerCreature player) 
+    {
+        _isFocus = true;
+        _player = player;
+        
+    }
+
+    public void OnUnfocus() 
+    {
+        _isFocus = false;
+        _hasInterected = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isFocus && _player != null)
+        {
+            if (Vector3.Distance(transform.position, _player.transform.position) < _interactionDistance && !_hasInterected)
+            {
+                Interect();
+            }
+        }
+    }
+
+    private void Interect()
+    {
+        _hasInterected = true;
+        Debug.Log("Interacted" + gameObject);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, _interactionDistance);
+    }
+}
---
+++ Laba_3/Assets/scripts/PlayerActionController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActionController : LivingCreatureActionController
+{
+    private PlayerCreature _playerCreature;
+    private Interacteble _lastTarget;
+    public PlayerActionController(PlayerCreature player) : base(player) 
+    {
+        _playerCreature = player;
+        _playerCreature.ServiceManager.inputController.LeftPoinrecClickHandler += OnLeftPointerCliked;
+
+    }
+
+    private void OnLeftPointerCliked(Vector3 destenation, Collider collider)
+    {
+
+        if (collider) {
+            Debug.Log("Col" + collider.gameObject);
+        }
+        if (_lastTarget != null)
+        {
+            _lastTarget.OnUnfocus();
+        }
+
+        if (collider != null) 
+        {
+            _lastTarget = collider.GetComponent<Interacteble>();
+            if (_lastTarget != null)
+            {
+                _lastTarget.OnFocus(_playerCreature);
+                Mouve(_lastTarget.transform.position, _lastTarget.StopingDIstance);
+                return;
+            }
+        }
+           
+
+        Mouve(destenation);
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        _playerCreature.ServiceManager.inputController.LeftPoinrecClickHandler -= OnLeftPointerCliked;
+    }
+}
